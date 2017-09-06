96ea44bf815bb44ef239067136f3d4f4e51fff3b
3203317 <3203317@qq.com>
2017-09-06 09:05:38 +0800

    haha

--- logon/src/login/views/pushCake/room.js
+++ logon/src/login/views/pushCake/room.js
@@ -15,10 +15,11 @@ var Room = function(opts){
   self._users   = {};
   self._players = {};
 
-  self._sum_user   = 10;
-  self._sum_player = 4;
-
   self._free_seat = [1, 2, 3, 4];
+
+  self.visitor_count = self.visitor_count || 6;     // 游客人数
+  self.fund          = opts.fund          || 1000;  // 组局基金
+  self.round_count   = opts.round_count   || 4;     // 圈数
 };
 
 var pro = Room.prototype;
@@ -41,12 +42,44 @@ pro.getUsers = function(){
 };
 
 /**
+ * 判断是否游戏是否开始
+ *
+ * @return
+ */
+pro.isStart = function(){
+  return this._free_seat.length <= this.getReadyCount();
+};
+
+/**
+ * 获取举手人数
+ *
+ * @return
+ */
+pro.getReadyCount = function(){
+  var count = 0;
+
+  for(let i of _.values(this._players)){
+    if(!!i.opts.ready) ++count;
+  }
+
+  return count;
+};
+
+/**
+ *
+ * @return
+ */
+pro.release = function(){
+  return 1 > _.size(this._users);
+};
+
+/**
  * 房间满了吗？
  *
  * @return boolean
  */
 pro.isFull = function(){
-  return this._sum_user <= _.size(this._users);
+  return (this._free_seat.length + self.visitor_count) <= _.size(this._users);
 };
 
 (function(){
@@ -75,9 +108,34 @@ pro.isFull = function(){
 
   function setSeat(user){
     var seat_no = this._free_seat.shift();
-    if(!seat_no) return;
+    if(!(0 < seat_no)) return;
 
-    this._players[user.id] = user;
+    this._players[seat_no] = user;
     user.opts.seat         = seat_no;
   }
 })();
+
+/**
+ * 退出群组
+ *
+ * @return
+ */
+pro.quit = function(user_id){
+  var self = this;
+
+  var user = self.getUser(user_id);
+  if(!user) return true;
+
+  if(self.isStart() && (0 < user.opts.seat)){
+    user.opts.quit_time = new Date().getTime();
+    user.opts.is_quit   = 1;
+    return false;
+  }
+
+  if(0 < user.opts.seat){
+    self._free_seat.push(user.opts.seat);
+    delete self._players[user.opts.seat];
+  }
+
+  return (delete self._users[user_id]);
+};
