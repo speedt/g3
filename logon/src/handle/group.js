6bebd84df3835d47d20f689e3afd37d316cd1fa4
3203317 <3203317@qq.com>
2017-09-07 17:22:27 +0800

    haha

--- logon/src/handle/group.js
+++ logon/src/handle/group.js
@@ -147,3 +147,48 @@ const _ = require('underscore');
     });
   }
 })();
+
+(() => {
+  /**
+   *
+   */
+  exports.re_entry = function(send, msg){
+    if(!_.isString(msg.body)) return logger.error('group re_entry empty');
+
+    try{ var data = JSON.parse(msg.body);
+    }catch(ex){ return; }
+
+    biz.group.re_entry(data.serverId, data.channelId)
+    .then (p1.bind(null, send, data))
+    .catch(p2.bind(null, send, data));
+  };
+
+  function p1(send, data, doc){
+    if(!doc) return;
+
+    var _data = [];
+    _data.push(null);
+    _data.push(JSON.stringify([3010, data.seqId, _.now(), doc[1]]));
+
+    for(let i of _.values(doc[0])){
+      if(!i.server_id || !i.channel_id) continue;
+      _data.splice(0, 1, i.channel_id);
+
+      send('/queue/back.send.v3.'+ i.server_id, { priority: 9 }, _data, (err, code) => {
+        if(err) return logger.error('group re_entry:', err);
+      });
+    }
+  }
+
+  function p2(send, data, err){
+    if('string' !== typeof err) return logger.error('group re_entry:', err);
+
+    var _data = [];
+    _data.push(data.channelId);
+    _data.push(JSON.stringify([3010, data.seqId, _.now(), , err]));
+
+    send('/queue/back.send.v3.'+ data.serverId, { priority: 9 }, _data, (err, code) => {
+      if(err) return logger.error('group re_entry:', err);
+    });
+  }
+})();
--- logon/src/handle/pushCake.js
+++ logon/src/handle/pushCake.js
@@ -20,6 +20,70 @@ const roomPool = require('emag.model').roomPool;
 
 (() => {
   /**
+   * 举手
+   *
+   * @return
+   */
+  exports.ready = function(send, msg){
+    if(!_.isString(msg.body)) return logger.error('channel ready empty');
+
+    try{ var data = JSON.parse(msg.body);
+    }catch(ex){ return; }
+
+    biz.pushCake.ready(data.serverId, data.channelId, next.bind(null, send, data))
+    .then (p1.bind(null, send, data))
+    .catch(p2.bind(null, send, data));
+  };
+
+  function p1(send, data, doc){
+    if(!doc) return;
+
+    var _data = [];
+    _data.push(null);
+    _data.push(JSON.stringify([3012, data.seqId, _.now(), doc[1]]));
+
+    for(let i of _.values(doc[0])){
+      if(!i.server_id || !i.channel_id) continue;
+      _data.splice(0, 1, i.channel_id);
+
+      send('/queue/back.send.v3.'+ i.server_id, { priority: 9 }, _data, (err, code) => {
+        if(err) return logger.error('channel ready:', err);
+      });
+    }
+  }
+
+  function p2(send, data, err){
+    if('string' !== typeof err) return logger.error('channel ready:', err);
+
+    var _data = [];
+    _data.push(data.channelId);
+    _data.push(JSON.stringify([3012, data.seqId, _.now(), , err]));
+
+    send('/queue/back.send.v3.'+ data.serverId, { priority: 9 }, _data, (err, code) => {
+      if(err) return logger.error('channel ready:', err);
+    });
+  }
+
+  function next(send, data, doc){
+    if(!doc) return;
+
+    var _data = [];
+    _data.push(null);
+    _data.push(JSON.stringify([doc[2], data.seqId, _.now(), doc[1]]));
+
+    for(let i of _.values(doc[0])){
+      if(!i.server_id || !i.channel_id) continue;
+      _data.splice(0, 1, i.channel_id);
+
+      send('/queue/back.send.v3.'+ i.server_id, { priority: 9 }, _data, (err, code) => {
+        if(err) return logger.error('channel ready '+ doc[2] +':', err);
+      });
+    }
+  }
+})();
+
+(() => {
+  /**
    *
    */
   exports.craps4 = function(send, msg){
@@ -29,7 +93,7 @@ const roomPool = require('emag.model').roomPool;
     }catch(ex){ return; }
 
     biz.pushCake.craps4(data.serverId, data.channelId)
-    .then(p1.bind(null, send, data))
+    .then (p1.bind(null, send, data))
     .catch(p2.bind(null, send, data));
   };
 
@@ -75,7 +139,7 @@ const roomPool = require('emag.model').roomPool;
     }catch(ex){ return; }
 
     biz.pushCake.bankerBet(data.serverId, data.channelId, data.data)
-    .then(p1.bind(null, send, data))
+    .then (p1.bind(null, send, data))
     .catch(p2.bind(null, send, data));
   };
 
@@ -121,7 +185,7 @@ const roomPool = require('emag.model').roomPool;
     }catch(ex){ return; }
 
     biz.pushCake.bankerCraps(data.serverId, data.channelId, next.bind(null, send, data))
-    .then(p1.bind(null, send, data))
+    .then (p1.bind(null, send, data))
     .catch(p2.bind(null, send, data));
   };
 
@@ -184,7 +248,7 @@ const roomPool = require('emag.model').roomPool;
     }catch(ex){ return; }
 
     biz.pushCake.unBankerBet(data.serverId, data.channelId, data.data)
-    .then(p1.bind(null, send, data))
+    .then (p1.bind(null, send, data))
     .catch(p2.bind(null, send, data));
   };
 
@@ -230,7 +294,7 @@ const roomPool = require('emag.model').roomPool;
     }catch(ex){ return; }
 
     biz.pushCake.bankerGoOn(data.serverId, data.channelId, data.data)
-    .then(p1.bind(null, send, data))
+    .then (p1.bind(null, send, data))
     .catch(p2.bind(null, send, data));
   };
 
